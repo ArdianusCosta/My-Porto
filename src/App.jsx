3041b077b57a9199a0645fbb8f9ec50db63a1c69
ArdianusCosta <costaaja017@gmail.com>
2025-06-03 13:02:05 +0700

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import DataImage from "./data"
+
 function App() {
 
   return (
     <>
-      <h1 className="text-4xl text-red-600 font-bold">hello</h1>
+      <div className="hero grid grid-cols-2">
+        <div>
+          <div>
+            <img src={DataImage.HeroImage} alt="Hero Image Costa" className="w-10 rounded-md" />
+            <q>Sesuatu yang indah, Lahir dari Ketekunan.😎</q>
+          </div>
+        </div>
+        <img src={DataImage.HeroImage} alt="Hero Image Costa" className="w-[500px]" />
+      </div>
     </>
   )
 }
